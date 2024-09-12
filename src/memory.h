8020a3e483bcb64555218cac5c406aa33d3d26f0
fcanlnony <1923170329@qq.com>
2024-09-12 22:52:48 +0800

    update header source

--- src/memory.h
+++ src/memory.h
@@ -1,6 +1,6 @@
 #ifndef FSHELL_MEMORY_H_
 #define FSHELL_MEMORY_H_
 
-unsigned int count_for_strlcpy(char *point);
+extern unsigned int count_for_strlcpy(char *point);
 
 #endif /*FSHELL_MEMORY_H_*/
