9ae5178bc6fd9a12626f91af664306ca0a4b0f5a
fcanlnony <1923170329@qq.com>
2024-09-21 17:33:20 +0800

    improve performance

--- src/memory.c
+++
@@ -1,6 +0,0 @@
-#include <string.h>
-
-unsigned int count_for_strlcpy(char *point)
-{
-  return (sizeof(char)*strlen(point)+sizeof(char));
-}
--- src/memory.h
+++ src/memory.h
@@ -1,6 +1,11 @@
 #ifndef FSHELL_MEMORY_H_
 #define FSHELL_MEMORY_H_
 
-extern unsigned int count_for_strlcpy(char *point);
+#include <string.h>
+
+static inline unsigned int count_for_strlcpy(char *point)
+{
+  return (sizeof(char)*strlen(point)+sizeof(char));
+}
 
 #endif /*FSHELL_MEMORY_H_*/
