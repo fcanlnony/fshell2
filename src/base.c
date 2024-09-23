aa5f429a020494e26706d22c81e74ad8d82ba838
fcanlnony <1923170329@qq.com>
2024-09-23 22:48:12 +0800

    improve performance

--- src/base.c
+++ src/base.c
@@ -10,17 +10,15 @@
 char* getusername()
 {
   struct passwd *pwd = getpwuid(getuid());
-  char *username = (char*)malloc(sizeof(char)*strlen(pwd->pw_name));
+  char *username = (char*)calloc(strlen(pwd->pw_name),sizeof(char));
   strlcpy(username,pwd->pw_name,count_for_strlcpy(pwd->pw_name));
   return username;
 }
 
 char* getcurrentdir()
 {
-  char *tmp_dir = malloc(sizeof(char)*MAX_DIR);
-  tmp_dir = getcwd(tmp_dir, MAX_DIR);
-  char *dir = (char*)malloc(sizeof(char)*strlen(tmp_dir));
-  strlcpy(dir,tmp_dir,count_for_strlcpy(tmp_dir));
+  char *dir = (char*)calloc(MAX_DIR,sizeof(char));
+  dir = getcwd(dir, MAX_DIR);
   return dir;
 }
 
@@ -44,7 +42,7 @@ char* getcurrentdir_b(user_t user)
 
 char* fshell_prompt_readline(char *username,char *currentdir,char *prompt)
 {
-  prompt = (char*)malloc(sizeof(char)*(strlen(username)+strlen(currentdir)+strlen(" ")*4+strlen("[]")+strlen(">")*3));
+  prompt = (char*)calloc((strlen(username)+strlen(currentdir)+strlen(" ")*4+strlen("[]")+strlen(">")*3),sizeof(char));
   sprintf(prompt, "%s [ %s ]\n>>> ",username,currentdir);
   return prompt;
 }
@@ -53,7 +51,7 @@ char* readline_history_path(char *username,char *path)
 {
   if(!strcmp(username,"root")) {
     path = (char*)calloc(strlen("/root/.fshell_historys"),sizeof(char));
-    strlcpy(path, "/root/.fshell_historys",count_for_strlcpy("/root/.fshell_historys"));
+    strncpy(path, "/root/.fshell_historys",sizeof(char)*strlen("/root/.fshell_historys"));
     return path;
   } else {
     path = (char*)calloc((strlen("/home//.fshell_historys")+strlen(username)),sizeof(char));
