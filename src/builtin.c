d45e94740e184cc5b45d90dc1dd432bd57e5502f
fcanlnony <1923170329@qq.com>
2024-11-02 23:14:50 +0800

    improve builtin command performance

--- src/builtin.c
+++ src/builtin.c
@@ -10,99 +10,116 @@
 
 short exec_builtin_cmd(char **array, const short FLAG, alias_t head, const char *username,const char *cd_history)
 {
-  if(FLAG == LIST_CMD_BUILTIN) {
-    if(array[1] == NULL || array[2] != NULL)
-      printf("fshell : list : unknown usage\n");
-    else if(!strcmp(array[1],"alias")) {
-      alias_t current = head;
-      if(current == NULL)
-	return -1;
-      unsigned short i = 0;
-      current = current->next;
-      if(current == NULL) {
-	printf("fshell : list : no alias\n");
-	return -1;
-      }
-      while(1) {
-	if(current->alias_name != NULL) {
-	  printf("%s => %s\n",current->alias_name,current->alias_command);
-	  i += 1;
+  switch(FLAG)
+    {
+    case LIST_CMD_BUILTIN: {
+      if(array[1] == NULL || array[2] != NULL)
+	printf("fshell : list : unknown usage\n");
+      else if(!strcmp(array[1],"alias")) {
+	alias_t current = head;
+	if(current == NULL)
+	  return -1;
+	unsigned short i = 0;
+	current = current->next;
+	if(current == NULL) {
+	  printf("fshell : list : no alias\n");
+	  return -1;
 	}
-	if(current->next != NULL)
-	  current = current->next;
-	else {
-	  printf("Total: %d\n",i);
-	  break;
+	while(1) {
+	  if(current->alias_name != NULL) {
+	    printf("%s => %s\n",current->alias_name,current->alias_command);
+	    i += 1;
+	  }
+	  if(current->next != NULL)
+	    current = current->next;
+	  else {
+	    printf("Total: %d\n",i);
+	    break;
+	  }
 	}
+      } else printf("fshell : %s : Invalid parameter\n",array[1]);
+      break;
+    }
+    case CD_CMD_BUILTIN: {
+      if(array[1] == NULL) {
+	printf("cd : dir -> null; stop\n");
+      } else if(array[2] != NULL)
+	printf("cd : too many parameters\n");
+      else if(!strncmp(array[1],"~",strlen("~")*sizeof(char))) {
+	if(!strcmp(username,"root")) {
+	  char *tmp_dir = (char*)calloc(strlen(array[1])-strlen("~")+strlen("/root"), sizeof(char));
+	  strlcpy(tmp_dir, "/root", count_for_strlcpy("/root"));
+	  strcat(tmp_dir, array[1]+strlen("~")*sizeof(char));
+	  chdir(tmp_dir);
+	  free(tmp_dir);
+	} else {
+	  char *tmp_dir = (char*)calloc(strlen(array[1])-strlen("~")+strlen(username)+strlen("/home/"),sizeof(char));
+	  strlcpy(tmp_dir, "/home/", count_for_strlcpy("/home/"));
+	  strcat(tmp_dir, username);
+	  strcat(tmp_dir, array[1]+strlen("~")*sizeof(char));
+	  chdir(tmp_dir);
+	  free(tmp_dir);
+	}
+      } else if(!strcmp(array[1],"....")) {
+	if(cd_history != NULL) {
+	  chdir(cd_history);
+	} else printf("fshell : cd : history path : null\n");
+      } else chdir(array[1]);
+      break;
+    }
+    case ALIAS_CMD_BUILTIN: {
+      if(array[2] == NULL || array[3] != NULL) {
+	printf("fshell : alias : unknown usage\n");
+	return -1;
       }
-    } else printf("fshell : %s : Invalid parameter\n",array[1]);
-  } else if(FLAG == CD_CMD_BUILTIN) {
-    if(array[1] == NULL) {
-      printf("cd : dir -> null; stop\n");
-    } else if(array[2] != NULL)
-      printf("cd : too many parameters\n");
-    else if(!strncmp(array[1],"~",strlen("~")*sizeof(char))) {
-      if(!strcmp(username,"root")) {
-	char *tmp_dir = (char*)calloc(strlen(array[1])-strlen("~")+strlen("/root"), sizeof(char));
-	strlcpy(tmp_dir, "/root", count_for_strlcpy("/root"));
-	strcat(tmp_dir, array[1]+strlen("~")*sizeof(char));
-	chdir(tmp_dir);
-	free(tmp_dir);
-      } else {
-	char *tmp_dir = (char*)calloc(strlen(array[1])-strlen("~")+strlen(username)+strlen("/home/"),sizeof(char));
-	strlcpy(tmp_dir, "/home/", count_for_strlcpy("/home/"));
-	strcat(tmp_dir, username);
-	strcat(tmp_dir, array[1]+strlen("~")*sizeof(char));
-	chdir(tmp_dir);
-	free(tmp_dir);
+      upload_alias_node(head, array[1], array[2]);
+      break;
+    }
+    case UNALIAS_CMD_BUILTIN: {
+      unsigned short n = 1;
+      while(array[n] != NULL) {
+	head = remove_alias_node(head, array[n]);
+	n += 1;
       }
-    } else if(!strcmp(array[1],"....")) {
-      if(cd_history != NULL) {
-	chdir(cd_history);
-      } else printf("fshell : cd : history path : null\n");
-    } else chdir(array[1]);
-  } else if(FLAG == ALIAS_CMD_BUILTIN) {
-    if(array[2] == NULL || array[3] != NULL) {
-      printf("fshell : alias : unknown usage\n");
-      return -1;
+      break;
     }
-    upload_alias_node(head, array[1], array[2]);
-  } else if(FLAG == UNALIAS_CMD_BUILTIN) {
-    unsigned short n = 1;
-    while(array[n] != NULL) {
-      head = remove_alias_node(head, array[n]);
-      n += 1;
+    case SET_ENV_CMD_BUILTIN: {
+      if(array[2] == NULL || array[3] != NULL) {
+	printf("fshell : set : unknown usage\n");
+	return -1;
+      }
+      char *tmp_env = (char*)calloc(strlen(array[1])+strlen(array[2])+strlen("="),sizeof(char));
+      strlcpy(tmp_env,array[1],count_for_strlcpy(array[1]));
+      strcat(tmp_env,"=");
+      strcat(tmp_env,array[2]);
+      putenv(tmp_env);
+      break;
     }
-  } else if(FLAG == SET_ENV_CMD_BUILTIN) {
-    if(array[2] == NULL || array[3] != NULL) {
-      printf("fshell : set : unknown usage\n");
-      return -1;
+    case UNSET_ENV_CMD_BUILTIN: {
+      unsigned short n = 1;
+      while(array[n] != NULL) {
+	unsetenv(array[n]);
+	n += 1;
+      }
+      break;
     }
-    char *tmp_env = (char*)calloc(strlen(array[1])+strlen(array[2])+strlen("="),sizeof(char));
-    strlcpy(tmp_env,array[1],count_for_strlcpy(array[1]));
-    strcat(tmp_env,"=");
-    strcat(tmp_env,array[2]);
-    putenv(tmp_env);
-  } else if(FLAG == UNSET_ENV_CMD_BUILTIN) {
-    unsigned short n = 1;
-    while(array[n] != NULL) {
-      unsetenv(array[n]);
-      n += 1;
+    case PRINT_CMD_BUILTIN: {
+      unsigned short n = 1;
+      while(array[n] != NULL) {
+	if(n >= 2)
+	  printf("\n");
+	char *tmp_put = getenv(array[n]),*tmp_put2 = getalias_command(head, array[n]);
+	if(tmp_put != NULL)
+	  printf("env : %s => %s\n",array[n],tmp_put);
+	else printf("env : %s : Invalid environment variable\n",array[n]);
+	if(tmp_put2 != NULL)
+	  printf("alias : %s => %s\n",array[n],tmp_put2);
+	else printf("alias : %s : Invalid alias variable\n",array[n]);
+	n += 1;
+      }
+      break;
     }
-  } else if(FLAG == PRINT_CMD_BUILTIN) {
-    unsigned short n = 1;
-    while(array[n] != NULL) {
-      if(n >= 2)
-	printf("\n");
-      char *tmp_put = getenv(array[n]),*tmp_put2 = getalias_command(head, array[n]);
-      if(tmp_put != NULL)
-	printf("env : %s => %s\n",array[n],tmp_put);
-      else printf("env : %s : Invalid environment variable\n",array[n]);
-      if(tmp_put2 != NULL)
-	printf("alias : %s => %s\n",array[n],tmp_put2);
-      else printf("alias : %s : Invalid alias variable\n",array[n]);
-      n += 1;
+    default:
+      return 0;
     }
-  }
-  return 0;
 }
