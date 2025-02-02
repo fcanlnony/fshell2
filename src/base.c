8bbcb0e4638d9a2a5832de1a84e0411405c7e91f
fcanlnony <1923170329@qq.com>
2025-02-02 11:40:51 +0800

    modified:   base.c : code security fixes

--- src/base.c
+++ src/base.c
@@ -32,7 +32,6 @@ user_t init_user_information(const char *username,char *userdir,user_t user)
 
 char* fshell_prompt_readline(const char *username,const char *currentdir,char *prompt)
 {
-  prompt = (char*)calloc((strlen(username)+strlen(currentdir)+strlen(" ")*4+strlen("[]")+strlen(">")*3+sizeof('\n')),sizeof(char));
   if(!strcmp(username,"root"))
     asprintf(&prompt, "\033[1;31m%s [ \033[0m%s\033[1;31m ]\033[0m\n>>> ", username, currentdir);
   else asprintf(&prompt, "\033[1;32m%s [ \033[0m%s\033[1;32m ]\033[0m\n>>> ", username, currentdir);
@@ -42,11 +41,9 @@ char* fshell_prompt_readline(const char *username,const char *currentdir,char *p
 char* readline_history_path(const char *username,char *path)
 {
   if(!strcmp(username,"root")) {
-    path = (char*)calloc(strlen("/root/.fshell_historys"),sizeof(char));
     asprintf(&path, "/root/.fshell_historys");
     return path;
   } else {
-    path = (char*)calloc((strlen("/home//.fshell_historys")+strlen(username)),sizeof(char));
     asprintf(&path, "/home/%s/.fshell_historys",username);
     return path;
   }
