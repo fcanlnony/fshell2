fe1db06293e517ce6604ce3072f02cfa885c49db
fcanlnony <1923170329@qq.com>
2025-02-08 12:35:32 +0800

    feat: improve user experience

--- src/builtin.c
+++ src/builtin.c
@@ -20,12 +20,12 @@ short exec_builtin_cmd(char **array, const short FLAG, head_alias_t head, const 
     {
     case LIST_CMD_BUILTIN: {
       if(array[1] == NULL || array[2] != NULL)
-	printf("fshell : list : unknown usage\n");
+	printf("fshell: list: unknown usage\n");
       else if(!strcmp(array[1],"alias")) {
 	alias_t current = head->next;
 	unsigned short i = 0;
 	if(current == NULL) {
-	  printf("fshell : list : no alias\n");
+	  printf("fshell: list: no alias\n");
 	  return -1;
 	}
 	while(1) {
@@ -40,38 +40,43 @@ short exec_builtin_cmd(char **array, const short FLAG, head_alias_t head, const 
 	    break;
 	  }
 	}
-      } else printf("fshell : %s : Invalid parameter\n",array[1]);
+      } else printf("fshell: %s : Invalid parameter\n",array[1]);
       break;
     }
     case CD_CMD_BUILTIN: {
       if(array[1] == NULL) {
-	printf("cd : dir -> null; stop\n");
+	printf("cd: dir -> null; stop\n");
       } else if(array[2] != NULL)
-	printf("cd : too many parameters\n");
+	printf("cd: too many parameters\n");
       else if(!strncmp(array[1],"~",strlen("~")*sizeof(char))) {
-	if(!strcmp(username,"root")) {
-	  char *tmp_dir = NULL;
+	char *tmp_dir = NULL;
+	if(!strcmp(username,"root"))
 	  asprintf(&tmp_dir, "/root/%s", array[1]+strlen("~")*sizeof(char));
-	  chdir(tmp_dir);
-	  if(tmp_dir != NULL)
-	    free(tmp_dir);
-	} else {
-	  char *tmp_dir = NULL;
+	else
 	  asprintf(&tmp_dir, "/home/%s/%s", username, array[1]+strlen("~")*sizeof(char));
-	  chdir(tmp_dir);
-	  if(tmp_dir != NULL)
+	if(access(tmp_dir, F_OK) == 0) {
+	  if(access(tmp_dir, X_OK) == 0)
+	    chdir(tmp_dir);
+	  else printf("fshell: cd: permission denied\n");
+	} else printf("fshell: cd: no such directory\n");
+	if(tmp_dir != NULL)
 	    free(tmp_dir);
-	}
       } else if(!strcmp(array[1],"....")) {
 	if(cd_history != NULL) {
 	  chdir(cd_history);
-	} else printf("fshell : cd : history path : null\n");
-      } else chdir(array[1]);
+	} else printf("fshell: cd: history path : null\n");
+      } else {
+	if(access(array[1], F_OK) == 0) {
+	  if(access(array[1], X_OK) == 0)
+	    chdir(array[1]);
+	  else printf("fshell: cd: permission denied\n");
+	} else printf("fshell: cd: no such directory\n");
+      }
       break;
     }
     case ALIAS_CMD_BUILTIN: {
       if(array[2] == NULL) {
-	printf("fshell : alias : unknown usage\n");
+	printf("fshell: alias: unknown usage\n");
 	return -1;
       }
       if(array[3] == NULL)
@@ -96,7 +101,7 @@ short exec_builtin_cmd(char **array, const short FLAG, head_alias_t head, const 
     }
     case SET_ENV_CMD_BUILTIN: {
       if(array[2] == NULL || array[3] != NULL) {
-	printf("fshell : set : unknown usage\n");
+	printf("fshell: set: unknown usage\n");
 	return -1;
       }
       static char tmp_env[FSHELL_ENV_SIZE];
@@ -119,18 +124,18 @@ short exec_builtin_cmd(char **array, const short FLAG, head_alias_t head, const 
 	  printf("\n");
 	char *tmp_put = getenv(array[n]),*tmp_put2 = getalias_command(head, array[n]);
 	if(tmp_put != NULL)
-	  printf("env : %s => %s\n",array[n],tmp_put);
-	else printf("env : %s : Invalid environment variable\n",array[n]);
+	  printf("env: %s => %s\n",array[n],tmp_put);
+	else printf("env: %s : Invalid environment variable\n",array[n]);
 	if(tmp_put2 != NULL)
-	  printf("alias : %s => %s\n",array[n],tmp_put2);
-	else printf("alias : %s : Invalid alias variable\n",array[n]);
+	  printf("alias: %s => %s\n",array[n],tmp_put2);
+	else printf("alias: %s : Invalid alias variable\n",array[n]);
 	n += 1;
       }
       break;
     }
     case LOAD_CMD_BUILTIN: {
       if(array[1] == NULL || array[2] != NULL)
-	printf("load : unknown usage");
+	printf("load: unknown usage\n");
       else {
 	char *file_path = array[1];
 	int size = filesize(file_path), line, getsize;
@@ -186,11 +191,11 @@ short exec_builtin_cmd(char **array, const short FLAG, head_alias_t head, const 
 	      int FLAG = check_builtin_cmd(array_content[0]);
 	      if(FLAG != NON_BUILTIN_CMD)
 		exec_builtin_cmd(array_content, FLAG, head, NULL, NULL);
-	      else printf("fshell: init file: error: %s FLAG %d\n", array_content[0], FLAG);
+	      else printf("fshell: load: error: %s FLAG %d\n", array_content[0], FLAG);
 	    }
 	  }
 	  fclose(fp);
-	}
+	} else printf("fshell: load: %s : no such file\n", array[1]);
       }
       break;
     }
